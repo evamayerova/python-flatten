93df71dbd515c3c3d27936c6b8795cbb178a9f1e
Eva Mayerova <eva.mayerova@firma.seznam.cz>
2017-04-13 13:49:37 +0200

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='flatten',
+    packages=['flatten'],
+    version='0.1',
+    description='Flatten any type of python object into one-level dict object',
+    author='Eva Mayerova',
+    author_email='eva.mayerova@seznam.cz',
+    url=
+    'https://github.com/peterldowns/mypackage',  # use the URL to the github repo
+    download_url=
+    'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',  # I'll explain this in a second
+    keywords=['dict', 'flatten'],
+    classifiers=[], )
